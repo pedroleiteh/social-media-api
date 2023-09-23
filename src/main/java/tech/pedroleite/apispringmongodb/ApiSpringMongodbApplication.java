48e35e58d1b05d2830f2f63f031e0b546d4bfcc1
Pedro Leite <contato.pedro77@gmail.com>
2023-09-23 01:02:50 -0300

    First commit

---
+++ src/main/java/tech/pedroleite/apispringmongodb/ApiSpringMongodbApplication.java
@@ -0,0 +1,13 @@
+package tech.pedroleite.apispringmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSpringMongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/pedroleite/apispringmongodb/ApiSpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.pedroleite.apispringmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
